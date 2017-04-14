c69d64864d1d436a3a48779e9ac513cac0a4eb3f
Jesper Findahl <jesper.findahl@gmail.com>
2017-04-14 18:56:09 +0200

    Removes hibernate files that or not used anymore.

--- application/src/main/resources/db/create-experiments-table.sql
+++
@@ -1,7 +0,0 @@
-create table EXPERIMENTS
-(USERNAME varchar(255) not null,
-EXPERIMENT_NAME varchar(255) not null,
-EXPERIMENT_NUMBER bigint not null,
-PERFORMED_ON timestamp,
-STATUS varchar(255) not null,
-primary key(USERNAME, EXPERIMENT_NAME, EXPERIMENT_NUMBER))
